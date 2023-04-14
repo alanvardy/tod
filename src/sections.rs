9a14031d27fcde1044a5321c07e68b73d468876e
Alan Vardy <alan@vardy.cc>
2023-04-14 06:44:28 -0700

    Add additional tests

--- src/sections.rs
+++ src/sections.rs
@@ -1,7 +1,7 @@
 use serde::Deserialize;
 
 // Projects are split into sections
-#[derive(Deserialize, Clone)]
+#[derive(PartialEq, Deserialize, Clone, Debug)]
 pub struct Section {
     pub id: String,
     pub project_id: String,
@@ -12,7 +12,35 @@ pub struct Section {
 pub fn json_to_sections(json: String) -> Result<Vec<Section>, String> {
     let result: Result<Vec<Section>, _> = serde_json::from_str(&json);
     match result {
-        Ok(body) => Ok(body),
+        Ok(sections) => Ok(sections),
         Err(err) => Err(format!("Could not parse response for item: {err:?}")),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::test;
+    use pretty_assertions::assert_eq;
+    /// Need to adjust this value forward or back an hourwhen timezone changes
+
+    #[test]
+    fn should_convert_json_to_sections() {
+        let sections = vec![
+            Section {
+                id: "1234".to_string(),
+                project_id: "5678".to_string(),
+                order: 1,
+                name: "Bread".to_string(),
+            },
+            Section {
+                id: "9012".to_string(),
+                project_id: "3456".to_string(),
+                order: 2,
+                name: "Meat".to_string(),
+            },
+        ];
+        let result = json_to_sections(test::responses::sections());
+        assert_eq!(result, Ok(sections));
     }
 }

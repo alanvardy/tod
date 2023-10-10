c3055c518dfd587108bbc01669753570c8d62179
Alan Vardy <alan@vardy.cc>
2023-10-09 18:36:30 -0700

    Update push aur

--- push_aur.sh
+++ push_aur.sh
@@ -3,9 +3,10 @@
 cd ../tod-bin/ || exit
 git pull
 cd ../tod/ || exit
+mv target/cargo-aur/PKGBUILD ./PKGBUILD
 makepkg --printsrcinfo > ../tod-bin/.SRCINFO
 mv PKGBUILD ../tod-bin/
-rm ./*.tar.gz
+rm target/cargo-aur/*.tar.gz
 cd ../tod-bin/ || exit
 git add .
 git commit -m "new version"

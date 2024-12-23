ae987aa14b43601b217949047b50dba1c9cdb328
Alan Vardy <alan@vardy.cc>
2024-12-23 12:47:06 -0800

    Refine scripts

--- scripts/push_aur.sh
+++ scripts/push_aur.sh
@@ -1,22 +1,28 @@
 #!/bin/bash
 
-echo "=== CHECKING THAT tod-bin FOLDER EXITS ===" &&
-cd ../tod-bin/ || exit &&
+if [ -z "${NAME}" ]; then
+  echo "Error: NAME environment variable is not set."
+  echo "Usage: NAME=tod VERSION=0.6.15 ./push_aur.sh"
+  exit 1
+fi
+
+echo "=== CHECKING THAT ${NAME}-bin FOLDER EXITS ===" &&
+cd "../${NAME}-bin/" || exit &&
 echo "=== PULLING LATEST AUR ===" &&
 git pull &&
-echo "=== CHECKING THAT tod FOLDER EXITS ===" &&
-cd ../tod/ || exit &&
+echo "=== CHECKING THAT ${NAME} FOLDER EXITS ===" &&
+cd "../${NAME}/" || exit &&
 echo "=== MOVING PKGBUILD ===" &&
 mv target/cargo-aur/PKGBUILD ./PKGBUILD
 echo "=== RUNNING MAKEPKG ===" &&
-makepkg --printsrcinfo > ../tod-bin/.SRCINFO &&
-mv PKGBUILD ../tod-bin/ &&
+makepkg --printsrcinfo > "../${NAME}-bin/.SRCINFO" &&
+mv PKGBUILD "../${NAME}-bin/" &&
 echo "=== DELETING TAR.GZ ===" &&
 rm target/cargo-aur/*.tar.gz &&
-cd ../tod-bin/ || exit &&
+cd "../${NAME}-bin/" || exit &&
 echo "=== PUSHING TO AUR ===" &&
 git add . &&
 git commit -m "v$VERSION" &&
 git push aur &&
-cd ../tod || exit &&
+cd "../${NAME}" || exit &&
 echo "=== SUCCESS ===" 

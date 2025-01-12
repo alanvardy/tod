3904735f33eb441d33d63e4e4cb06b6fee70d2b3
Alan Vardy <alan@vardy.cc>
2025-01-12 14:31:24 -0800

    Fix comments

--- src/comment.rs
+++ src/comment.rs
@@ -12,27 +12,74 @@ pub struct Comment {
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone, Eq, PartialEq)]
-pub struct Attachment {
+#[serde(untagged)]
+pub enum Attachment {
+    File(FileAttachment),
+    Url(UrlAttachment),
+    Video(VideoAttachment),
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone, Eq, PartialEq)]
+pub struct FileAttachment {
     pub file_name: String,
     pub file_type: String,
     pub file_url: String,
     pub resource_type: String,
 }
+
+#[derive(Serialize, Deserialize, Debug, Clone, Eq, PartialEq)]
+pub struct UrlAttachment {
+    description: String,
+    favicon: String,
+    image: String,
+    image_height: u32,
+    image_width: u32,
+    resource_type: String,
+    site_name: String,
+    title: String,
+    url: String,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone, Eq, PartialEq)]
+pub struct VideoAttachment {
+    html: String,
+    resource_type: String,
+    title: String,
+    url: String,
+    site_name: String,
+}
+
 impl Comment {
     pub fn fmt(&self, config: &Config) -> Result<String, Error> {
         let timezone = time::timezone_from_str(&config.timezone)?;
         let datetime = time::datetime_from_str(&self.posted_at, timezone)?;
         let formatted_date = time::format_datetime(&datetime, config)?;
 
-        let link = if let Some(Attachment {
-            file_name,
-            file_url,
-            ..
-        }) = self.attachment.clone()
-        {
-            format!("\nAttachment \x1B]8;;{file_url}\x1B\\[{file_name}]\x1B]8;;\x1B\\")
-        } else {
-            String::new()
+        let link = match &self.attachment {
+            None => String::new(),
+            Some(Attachment::Url(UrlAttachment {
+                url,
+                site_name,
+                title,
+                ..
+            })) => {
+                format!("\nAttachment \x1B]8;;{url}\x1B\\[{site_name}: {title}]\x1B]8;;\x1B\\")
+            }
+            Some(Attachment::Video(VideoAttachment {
+                url,
+                site_name,
+                title,
+                ..
+            })) => {
+                format!("\nAttachment \x1B]8;;{url}\x1B\\[{site_name}: {title}]\x1B]8;;\x1B\\")
+            }
+            Some(Attachment::File(FileAttachment {
+                file_name,
+                file_url,
+                ..
+            })) => {
+                format!("\nAttachment \x1B]8;;{file_url}\x1B\\[{file_name}]\x1B]8;;\x1B\\")
+            }
         };
         Ok(format!(
             "Posted {}{}\n{}",

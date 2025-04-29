3be493508d920257c63ae10fc52e332b45c8033f
Alan Vardy <alanvardy@gmail.com>
2025-04-28 17:36:00 -0700

    Get user data from v1 endpoint

--- src/user.rs
+++ src/user.rs
@@ -1,11 +1,5 @@
 use serde::Deserialize;
 
-/// https://developer.todoist.com/sync/v9/#user
-#[derive(Deserialize, Debug, PartialEq, Eq)]
-pub struct SyncResponse {
-    pub user: User,
-}
-
 #[derive(Deserialize, Debug, PartialEq, Eq)]
 pub struct User {
     pub tz_info: TzInfo,

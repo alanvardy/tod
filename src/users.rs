bb4bdad7b1716ab5750d4186e2908962c69d5be6
Jesse <stacksjb@users.noreply.github.com>
2025-05-16 16:58:31 -0600

    Script commentary cleanup

--- src/users.rs
+++ src/users.rs
@@ -5,13 +5,15 @@ use serde::Deserialize;
 pub struct User {
     pub tz_info: TzInfo,
 }
-
+// This file is used to pull the user information (timezone) from the Todoist API
 #[derive(Deserialize, Debug, PartialEq, Eq)]
 pub struct TzInfo {
     pub timezone: String,
 }
 
 pub fn json_to_user(json: String) -> Result<User, Error> {
+    // Deserializes JSON string into a `User` struct using Serde.
+    // Returns an error if the JSON string does not match the `User` struct format.
     let user: User = serde_json::from_str(&json)?;
     Ok(user)
 }
